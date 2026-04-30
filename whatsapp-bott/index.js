c325fc2af81cb646e1ed0bb7bb39d75e83613f0f
Shahzill Naveed <shahzill.naveed@ucalgary.ca>
2026-04-29 18:26:11 -0600

    playing 11

--- whatsapp-bott/index.js
+++ whatsapp-bott/index.js
@@ -408,7 +408,7 @@ const sendTeaserMessage = async () => {
   } catch {}
 };
 
-cron.schedule("25 18 * * *", sendDailyReminders, {
+cron.schedule("28 18 * * *", sendDailyReminders, {
   timezone: "America/Edmonton",
 });
 
